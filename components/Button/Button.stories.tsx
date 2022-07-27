3719b4ee6756e58ca8df99911cf42b41b3172e8a
Ricardo Zirk <rzirk@users.noreply.github.com>
2022-07-27 23:07:27 +0200

    Added source code plugin

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 
 import {NButton} from './Button';
+import { Box, NativeBaseProvider } from 'native-base';
 
 export default {
   title: 'components/Button',
@@ -10,7 +11,11 @@ export default {
 } as ComponentMeta<typeof NButton>;
 
 export const Basic: ComponentStory<typeof NButton> = args => (
-  <NButton {...args} />
+  <NativeBaseProvider>
+    <Box alignItems="center">
+      <NButton {...args} />
+    </Box>
+  </NativeBaseProvider>
 );
 
 Basic.args = {
